606c18811f9c9e44dedfc12165a8489acf252108
duydinhnguyen <1520398@gm.uit.edu.vn>
2024-10-17 22:36:32 +0700

    update

--- frontend/src/components/Suggestion/SuggestionItem.jsx
+++ frontend/src/components/Suggestion/SuggestionItem.jsx
@@ -1,17 +1,20 @@
 /* eslint-disable react/prop-types */
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import styles from './Suggestion.module.css'
 import { getVideoAction } from '../../Redux/Actions/QueryVideoActions'
 
 function SuggestionItem(props) {
-    const { mode } = useSelector((state) => state.queryVideoSlice);
+    // const { mode } = useSelector((state) => state.queryVideoSlice);
     const dispatch = useDispatch()
     const { name } = props
 
     const handleQuery = () => {
+        const formData = new FormData()
+        formData.append('query', name)
+        formData.append('image', null)
         dispatch(getVideoAction({
-            query: name,
-            mode
+            query: "",
+            formData
         }))
     }
 
